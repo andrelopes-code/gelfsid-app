66bfcd0933d8f6b43111a2a7ca58a74f69e01eba
André Lopes <andrelopes-code@gmail.com>
2024-12-01 13:03:39 -0300

    adicionadas funções utilitarias de tempo e data

---
+++ map/utils/timeutils.py
@@ -0,0 +1,65 @@
+from datetime import timedelta
+
+from django.utils.timezone import now
+
+
+def days_ago(days: int):
+    """
+    Retorna a data/hora de 'X' dias atrás.
+    """
+    return now() - timedelta(days=days)
+
+
+def days_from_now(days: int):
+    """
+    Retorna a data/hora de 'X' dias no futuro.
+    """
+    return now() + timedelta(days=days)
+
+
+def months_ago(months: int):
+    """
+    Retorna a data/hora de 'X' meses atrás.
+    Considera que um mês tem 30 dias para simplificação.
+    """
+    return now() - timedelta(days=30 * months)
+
+
+def months_from_now(months: int):
+    """
+    Retorna a data/hora de 'X' meses no futuro.
+    Considera que um mês tem 30 dias para simplificação.
+    """
+    return now() + timedelta(days=30 * months)
+
+
+def years_ago(years: int):
+    """
+    Retorna a data/hora de 'X' anos atrás.
+    Considera que um ano tem 365 dias.
+    """
+    return now() - timedelta(days=365 * years)
+
+
+def years_from_now(years: int):
+    """
+    Retorna a data/hora de 'X' anos no futuro.
+    Considera que um ano tem 365 dias.
+    """
+    return now() + timedelta(days=365 * years)
+
+
+def start_of_today():
+    """
+    Retorna o início do dia atual (00:00:00).
+    """
+    current = now()
+    return current.replace(hour=0, minute=0, second=0, microsecond=0)
+
+
+def end_of_today():
+    """
+    Retorna o final do dia atual (23:59:59).
+    """
+    current = now()
+    return current.replace(hour=23, minute=59, second=59, microsecond=999999)
