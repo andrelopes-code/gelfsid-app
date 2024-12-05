13dd1bee9e6c4d0e9ec37d55363d2665b49f6a36
André Lopes <andrelopes-code@gmail.com>
2024-12-05 20:28:35 -0300

    adicionado favicon e um css customizado a pagina de admin

--- map/urls.py
+++ map/urls.py
@@ -1,4 +1,6 @@
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 from . import views
 
@@ -6,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('grappelli/', include('grappelli.urls')),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('assets/favicon.ico')))
 ]
