9fc0119034033c8fdd4f0c562b4f9cce253dc7a8
André Lopes <andrelopes-code@gmail.com>
2024-12-15 13:26:26 -0300

    run.sh

--- run.sh
+++ run.sh
@@ -28,10 +28,6 @@ else
     echo "Dependências do Node.js já instaladas."
 fi
 
-python manage.py collectstatic --noinput
-python manage.py migrate
-python manage.py fixtures
-
 echo "Construindo o projeto..."
 npm run build
 
